b249f0ea450553cf2c68f0e9da52583d7cf078f7
SaraMiri <saramiragaya@gmail.com>
2019-12-22 00:44:15 +0100

    Finished

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+var personasRouter = require('./routes/personas-routes')
+
+// Incluir el fichero con la definición de la BD
+var db = require('./db/db');
+
+// Conectar con la BD
+db.connect('mongodb://localhost:27017', function (err) {
+    if (err) {
+        throw ('Fallo en la conexión con la BD');
+    }
+});
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+app.use('/api/personas', personasRouter)
+
+module.exports = app;
